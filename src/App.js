65cd7d8b2632742c8ac9e98b4cd29dcc9687ed7d
Saminuddin Shaikh <saminuddins2@gmail.com>
2022-07-29 00:37:44 +0530

    data trans

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import NoteState from "./context/notes/NotesState";
+import About from "./pages/about";
+import Home from "./pages/home";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is cloud book</h1>
-    </div>
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/about" element={<About />} />
+            </Routes>
+          </div>
+        </Router>
+      </NoteState>
+    </>
   );
 }
 
